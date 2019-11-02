8bebf721f61ee812081347a56dd98f8c5eeba8bd
Shorh <Shorhina@gmail.com>
2019-11-02 16:13:18 +0500

    Установка нужных библиотек + установка драйвера БД для Windows

--- Отчеты Excel/Первый запуск.py
+++ Отчеты Excel/Первый запуск.py
@@ -1,5 +1,6 @@
 from subprocess import call
 from os.path import join
+from os import environ, name, getcwd
 
 
 def install(package):
@@ -11,3 +12,11 @@ path = join('data', 'requirements.txt')
 with open(path, encoding='utf8') as fin:
     for line in fin:
         install(line[:-1])
+
+if name == 'nt':
+    if 'PROGRAMFILES(X86)' in environ:
+        file = join('data', 'msodbcsql_17.4.1.1_x64.msi')
+    else:
+        file = join('data', 'msodbcsql_17.4.1.1_x86.msi')
+    call(['msiexec', '/i', file],
+         shell=True)
