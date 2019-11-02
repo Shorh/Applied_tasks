4ee2bcca0b97174d035c1b5137fce28e3a81e396
Shorh <Shorhina@gmail.com>
2019-11-02 16:36:14 +0500

    Модификация первого запуска

--- Отчеты Excel/First_start/First_start.py
+++ Отчеты Excel/First_start/First_start.py
@@ -1,13 +1,13 @@
 from subprocess import call
 from os.path import join
-from os import environ, name, getcwd
+from os import environ, name
 
 
 def install(package):
     call(['pip', 'install', package], shell=True)
 
 
-path = join('data', 'requirements.txt')
+path = join('requirements.txt')
 
 with open(path, encoding='utf8') as fin:
     for line in fin:
@@ -15,8 +15,8 @@ with open(path, encoding='utf8') as fin:
 
 if name == 'nt':
     if 'PROGRAMFILES(X86)' in environ:
-        file = join('data', 'msodbcsql_17.4.1.1_x64.msi')
+        file = join('msodbcsql_17.4.1.1_x64.msi')
     else:
-        file = join('data', 'msodbcsql_17.4.1.1_x86.msi')
+        file = join('msodbcsql_17.4.1.1_x86.msi')
     call(['msiexec', '/i', file],
          shell=True)
